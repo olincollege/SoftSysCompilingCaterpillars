90e2ec8d49a4d9df91fce42d1a7fdadb7e198761
Abitamim <abitamimbharmal@hotmail.com>
2022-03-11 01:55:41 -0500

    Should be final lexer

--- src/lexer.c
+++ src/lexer.c
@@ -12,17 +12,22 @@ int last_char = ' ';
 
 T_token get_token() {
     memset(identifier_str, 0, LENGTH);
-
-    T_token token = create_single_char_token(last_char);
-    if (token) {
-        return token;
-    }
-    while (isspace(last_char)) {
+    while (isspace(last_char) || last_char == '(' || last_char == ')' || last_char == '=') {
+        // puts("space");
         if (last_char == '\n') {
+            last_char = getchar();
             return create_nl_token();
         }
         last_char = getchar();
     }
+    // puts("first");
+    // printf("%c \n", last_char);
+    // T_token token = create_single_char_token(last_char);
+    // if (token) {
+    //     last_char = getchar();
+    //     return token;
+    // }
+
     if (isalpha(last_char)) {
         // puts("alpha");
         int i = 0;
@@ -31,11 +36,24 @@ T_token get_token() {
             identifier_str[i++] = last_char;
         }
         identifier_str[i] = '\0';
-        return create_val_token(identifier_str);
+        if (!strcmp(identifier_str, "while") || !strcmp(identifier_str, "endwhile")) {
+            // puts("while enter");
+            return create_while_token(identifier_str);
+        }
+        // printf("comp: %d \n", !strcmp(identifier_str, "if"));
+        if (!strcmp(identifier_str, "if") || !strcmp(identifier_str, "else") || !strcmp(identifier_str, "endif")) {
+            // puts("if enter");
+            return create_if_token(identifier_str);
+        }
+
+        return create_var_token(identifier_str);
     }
+
+    // puts("second");
 
     //doesn't work with multiple periods i.e. 1.234.533
     if (isdigit(last_char) || last_char == '.') {
+        // puts("beta");
         char num_str[LENGTH];
         memset(num_str, 0, LENGTH);
         int i = 0;
@@ -51,7 +69,8 @@ T_token get_token() {
             num_str[i] = '.';
         }
         num_val = strtod(num_str, 0);
-        return create_number_token(num_val);;
+        // printf("digit: %f \n", num_val);
+        return create_number_token(num_val);
     }
 
     if (last_char == '#') {
@@ -68,58 +87,86 @@ T_token get_token() {
         return create_end_token();
     }
 
-    while (isspace(last_char)) {
+    while (isspace(last_char) || last_char == '(' || last_char == ')') {
+        // puts("camma");
         if (last_char == '\n') {
+            last_char = getchar();
             return create_nl_token();
         }
         last_char = getchar();
     }
 
     T_token token = create_single_char_token(last_char);
-    if (token) {
+    last_char = getchar();
+    if (token != NULL) {
         return token;
     }
-    last_char = getchar();
 }
 
 T_token create_single_char_token(char c) {
-    T_token token = malloc(sizeof(T_token));
+    T_token token = malloc(sizeof(*token));
+    T_oper oper = malloc(sizeof(*oper));
+    T_comp comp = malloc(sizeof(*comp));
+
     switch (c) {
         case '+':
             token->type = OPER;
-            token->value.oper->type = ADD;
+            oper->type = ADD;
+            token->value.oper = oper;
+            free_token(comp);
             break;
         case '-':
             token->type = OPER;
-            token->value.oper->type = SUB;
+            oper->type = SUB;
+            token->value.oper = oper;
+            free_token(comp);
             break;
         case '*':
             token->type = OPER;
-            token->value.oper->type = MULT;
+            oper->type = MULT;
+            token->value.oper = oper;
+            free_token(comp);
             break;
         case '/':
             token->type = OPER;
-            token->value.oper->type = DIV;
+            oper->type = DIV;
+            token->value.oper = oper;
+            free_token(comp);
             break;
         case '>':
-            token->type = OPER;
-            token->value.oper->type = GT;
+            token->type = COMP;
+            comp->type = GT;
+            token->value.comp = comp;
+            free_token(oper);
             break;
         case '<':
-            token->type = OPER;
-            token->value.oper->type = LT;
+            token->type = COMP;
+            comp->type = LT;
+            token->value.comp = comp;
+            free_token(oper);
             break;
-        case '=':
-            token->type = OPER;
-            token->value.oper->type = EQ;
+        case '~':
+            token->type = COMP;
+            comp->type = EQ;
+            token->value.comp = comp;
+            free_token(oper);
             break;
         case '!': //val1 ! val2 means val1 != val2
-            token->type = OPER;
-            token->value.oper->type = NEQ;
+            token->type = COMP;
+            comp->type = NEQ;
+            token->value.comp = comp;
+            free_token(oper);
+            break;
         default:
-            return;
+            free_token(oper);
+            free_token(comp);
+            return NULL;
     }
     return token;
+}
+
+void free_token(void *pointer) {
+    free(pointer);
 }
 
 T_token create_end_token() {
@@ -128,20 +175,43 @@ T_token create_end_token() {
     return token;
 }
 T_token create_var_token(char* val) {
-    T_token token = malloc(sizeof(T_token));
+    T_token token = malloc(sizeof(*token));
     token->type = VAL;
+    T_val val_token = malloc(sizeof(*val_token));
+    token->value.val = val_token;
     token->value.val->type = VAR;
-    token->value.val->value.var = malloc(sizeof(val));
-    strcpy(val, token->value.val->value.var);
+    token->value.val->value.var = malloc((strlen(val) + 1));
+    strcpy(token->value.val->value.var, val);
 
     return token;
 }
 T_token create_number_token(double n) {
-    T_token token = malloc(sizeof(T_token));
+    T_token token = malloc(sizeof(*token));
     token->type = VAL;
+    T_val val_token = malloc(sizeof(*val_token));
+    token->value.val = val_token;
     token->value.val->type = NUM;
-    *token->value.val->value.var = n;
+    token->value.val->value.num = malloc(2);
+    *token->value.val->value.num = n;
+    // printf("create: %f \n", *token->value.val->value.num);
+    return token;
+}
 
+T_token create_while_token(char* val) {
+    T_token token = malloc(sizeof(*token));
+    token->type = T_WHILE;
+    T_while while_token = malloc(sizeof(*while_token));
+    token->value.t_while = while_token;
+    token->value.t_while->type=((!strcmp(val, "while"))?WHILE:ENDWHILE);
+    return token;
+}
+
+T_token create_if_token(char* val) {
+    T_token token = malloc(sizeof(*token));
+    token->type = T_IF;
+    T_if if_token = malloc(sizeof(*if_token));
+    token->value.t_if = if_token;
+    token->value.t_if->type=(!strcmp(val, "if"))?WHILE:(!strcmp(val, "else")?ELSE:ENDIF);
     return token;
 }
 
@@ -150,15 +220,26 @@ T_token create_nl_token() {
     token->type = NL;
     return token;
 }
-// int main(int argc, char *argv[]) {
-//     int r;
-//     while ((r = get_token()) != -1) {
-//         if (r == tok_identifier || r == tok_def) {
-//             puts(identifier_str);
-//         } else if (r == tok_number) {
-//             printf("%f \n", num_val);
-//         } else {
-//             printf("%c \n", r);    
-//         }
-// }
-// }   
+
+int main(int argc, char *argv[]) {
+    T_token r;
+    while ((r = get_token())->type != END) {
+        if (r->type == VAL) {
+            if (r->value.val->type == NUM) {
+                printf("type: %d type: %d  value: %f \n", r->type, r->value.val->type, *r->value.val->value.num);
+            } else {
+                printf("type: %d type: %d value: %s \n", r->type, r->value.val->type, r->value.val->value.var); 
+            }
+        } else if (r->type == NL) {
+            puts("NL");
+        } else if (r->type == COMP) {
+            printf("type: %d  value: %d \n", r->type, r->value.comp->type);
+        } else if (r->type == OPER) {
+            printf("type: %d  value: %d \n", r->type, r->value.oper->type);
+        } else if (r->type == T_IF) {
+            printf("if type: %d \n", r->value.t_if->type);
+        } else if (r->type == T_WHILE) {
+            printf("while type: %d \n", r->value.t_while->type);
+        }
+}
+}   
--- src/lexer.h
+++ src/lexer.h
@@ -1,4 +1,4 @@
-#ifndf LEXER_H
+#ifndef LEXER_H
 #define LEXER_H
 
 #define LENGTH 255
@@ -7,13 +7,17 @@ typedef struct S_token* T_token;
 typedef struct S_val* T_val;
 typedef struct S_oper* T_oper;
 typedef struct S_comp* T_comp;
+typedef struct S_while* T_while;
+typedef struct S_if* T_if;
 
 struct S_token {
-    enum {VAL, OPER, COMP, NL, END} type;//NL is new line, END is end
+    enum {VAL, OPER, COMP, NL, END, T_WHILE, T_IF} type;//NL is new line, END is end
     union {
         T_val val;
         T_oper oper;
         T_comp comp;
+        T_while t_while;
+        T_if t_if;
     } value;
 };
 
@@ -37,11 +41,24 @@ struct S_comp {
     } type;
 };
 
+struct S_while {
+    enum {
+        WHILE, ENDWHILE
+    } type;
+};
+
+struct S_if {
+    enum {
+        IF, ELSE, ENDIF
+    } type;
+};
+
+void free_token(void *pointer);
 T_token create_end_token();
 T_token create_var_token(char* val);
+T_token create_while_token(char* val);
+T_token create_if_token(char* val);
 T_token create_number_token(double n);
-T_token create_operator_token(char c);
-T_token create_comp_token(char c);
 T_token create_nl_token();
 T_token create_single_char_token(char c);
 
