406a7430bbb6d4a1f1b83dcedda027a24a5fe39d
Rishita Sarin <rsarin@olin.edu>
2022-03-18 21:42:20 -0400

    commented lexer.h

--- src/lexer.h
+++ src/lexer.h
@@ -3,6 +3,7 @@
 
 #define LENGTH 255
 
+//save tokens as structs so that we can save all the information regarding each token (various data types) in one place
 typedef struct S_token* T_token;
 typedef struct S_val* T_val;
 typedef struct S_oper* T_oper;
@@ -11,7 +12,8 @@ typedef struct S_while* T_while;
 typedef struct S_if* T_if;
 
 
-struct S_token {
+struct S_token { 
+    // enum identifies what type of token it is
     enum {VAL, OPER, COMP, NL, END, T_WHILE, T_IF} type;//NL is new line, END is end
     union {
         T_val val;
@@ -22,7 +24,7 @@ struct S_token {
     } value;
 };
 
-struct S_val {
+struct S_val { //number and variable 
     enum {NUM, VAR} type;
     union {
         double *num;
@@ -30,31 +32,31 @@ struct S_val {
     } value;
 };
 
-struct S_oper {
+struct S_oper { //operators: + - * /
     enum{
         ADD, SUB, MULT, DIV
     } type;
 };
 
-struct S_comp {
+struct S_comp { //comparison operators: = > < !
     enum {
         EQ, LT, GT, NEQ
     } type;
 };
 
-struct S_while {
+struct S_while { //while and endwhile
     enum {
         WHILE, ENDWHILE
     } type;
 };
 
-struct S_if {
+struct S_if { //if, else, and endif
     enum {
         IF, ELSE, ENDIF
     } type;
 };
 
-void free_token(void *pointer);
+void free_token(void *pointer); 
 T_token create_end_token();
 T_token create_var_token(char* val);
 T_token create_while_token(char* val);
