d6b0dcfe6f6facf572450c944926976c7b557ade
Rishita Sarin <rsarin@olin.edu>
2022-03-18 22:31:52 -0400

    commented parser.c

--- src/parser.c
+++ src/parser.c
@@ -3,7 +3,7 @@
 
 T_token lookahead_token;
 
-T_token get_lookahead() {
+T_token get_lookahead() { //look at the next token without discarding/chaging the current token 
     return lookahead_token;
 }
 
@@ -20,56 +20,56 @@ void next_token() {
  * @return T_statement_list 
  */
 T_statement_list parse_statement_list(int in_loop_while) {
-    if (END != get_lookahead() -> type) {
+    if (END != get_lookahead() -> type) { //if next token is not "END", allocate memory for the statement list and let current list equal it
         T_statement_list list = malloc(sizeof(*list));
         T_statement_list cur_list = list;
         // puts("26");
         int run = 0;
         while (1) {
 
-            if (get_lookahead() -> type == T_WHILE)  {
-                if (get_lookahead() -> value.t_while -> type == ENDWHILE) {
+            if (get_lookahead() -> type == T_WHILE)  { //if next token is a "WHILE" token
+                if (get_lookahead() -> value.t_while -> type == ENDWHILE) {// if next token is an "ENDWHILE" token, get next token and break
                     next_token();
                     if (in_loop_while != 2) parser_error();
                     break;
-                } else {
-                if (run++ != 0) {
-                    cur_list -> statement_list = malloc(sizeof(*cur_list));
-                    cur_list = cur_list -> statement_list;
+                } else { //if next token is not an "ENDWHILE" token
+                if (run++ != 0) { 
+                    cur_list -> statement_list = malloc(sizeof(*cur_list)); //append memory allocated to current list
+                    cur_list = cur_list -> statement_list; //update current list
                 }
-                    cur_list -> type = LOOP;
-                    cur_list -> statement.loop = parse_while();
+                    cur_list -> type = LOOP; //set current list type to loop 
+                    cur_list -> statement.loop = parse_while(); //parse through the while statement
                 }
             }
-            else if (get_lookahead() -> type == T_IF)  {
+            else if (get_lookahead() -> type == T_IF)  {//if next token is an "IF" token
 
-                if (get_lookahead() -> value.t_if -> type == ELSE) {
+                if (get_lookahead() -> value.t_if -> type == ELSE) { //if the next token is an "ELSE" token, break
                     if (in_loop_while != 1) parser_error();
                     break;
-                } else if (get_lookahead() -> value.t_if -> type == ENDIF) {
+                } else if (get_lookahead() -> value.t_if -> type == ENDIF) { //if next token is an "ENDIF" token, get next token and break
                     next_token();
                     if (in_loop_while != 1 && in_loop_while != 3) parser_error();
                     break;
                 } else {
                     if (run++ != 0) {
-                    cur_list -> statement_list = malloc(sizeof(*cur_list));
-                    cur_list = cur_list -> statement_list;
+                    cur_list -> statement_list = malloc(sizeof(*cur_list)); //append memory allocated to current list
+                    cur_list = cur_list -> statement_list; //update current list
                     }
-                    cur_list -> type = BRANCH;
-                    cur_list -> statement.branch = parse_branch();
+                    cur_list -> type = BRANCH; //set current list type to branch
+                    cur_list -> statement.branch = parse_branch(); //parse through the branch 
                 }
-            } else if (get_lookahead() -> type == VAL) {
+            } else if (get_lookahead() -> type == VAL) { //if next token is a value
                 if (run++ != 0) {
-                    cur_list -> statement_list = malloc(sizeof(*cur_list));
-                    cur_list = cur_list -> statement_list;
+                    cur_list -> statement_list = malloc(sizeof(*cur_list)); //append memory allocated to current list
+                    cur_list = cur_list -> statement_list; //update current list
                 }
-                cur_list -> type = STATE;
-                cur_list -> statement.statement = parse_statement();
-            } else if (get_lookahead() -> type == END) {
+                cur_list -> type = STATE; //set current list type to statement
+                cur_list -> statement.statement = parse_statement(); //parse through the statement
+            } else if (get_lookahead() -> type == END) { //if next token is "END", break
                 break;
             }
         }
-        return list;
+        return list; 
     } else {
         printf("exit inloopwhile: %d \n", in_loop_while);
         if (in_loop_while != 0) parser_error();
@@ -89,6 +89,7 @@ T_statement_list create_statement_list(int type, T_statement statement, T_statem
         // new_list->statement.loop = deep_copy_statement(statement);
     } else {
         new_list -> type = STATE;
+        //replace with deep_copy_statement
         new_list->statement.statement = deep_copy_statement(statement);
     }
     
@@ -96,7 +97,7 @@ T_statement_list create_statement_list(int type, T_statement statement, T_statem
     return new_list;
 }
 
-T_branch parse_branch() {
+T_branch parse_branch() { //branch = conditional + statement_list + (if there is an "ELSE" statement, add another branch to parse through its statement_list)
     T_token if_token = get_lookahead();
     // printf("%d \n", if_token -> type);
     if (T_IF != if_token -> type || IF != if_token -> value.t_if -> type) parser_error();
@@ -120,7 +121,7 @@ T_branch create_branch(T_conditional conditional, T_statement_list if_statements
     return new_branch;
 }
 
-T_loop parse_while() {
+T_loop parse_while() { //while = conditional + statement_list 
     T_token while_token = get_lookahead();
     if (T_WHILE != while_token -> type || WHILE != while_token -> value.t_while -> type)
         parser_error();
@@ -140,7 +141,7 @@ T_loop create_while(T_conditional conditional, T_statement_list list) {
     return new_loop;
 }
 
-T_conditional parse_conditional() {
+T_conditional parse_conditional() { //conditional = lhs(left hand side) expression + comparator + rhs (right hand side) expression
     T_token token = get_lookahead();
     if (VAL != token -> type) parser_error();
     T_expression lhs = parse_expression();
@@ -154,13 +155,13 @@ T_conditional parse_conditional() {
 
 T_conditional create_conditional(T_expression lhs, T_comp comparator, T_expression rhs) {
     T_conditional new_conditional = malloc(sizeof(*new_conditional));
-    new_conditional -> lhs = lhs;
-    new_conditional -> comparator = deep_copy_comp(comparator);
-    new_conditional -> rhs = rhs;
+    new_conditional -> lhs = lhs; //since lhs expression is already heap-allocated, can directly assign reference
+    new_conditional -> comparator = deep_copy_comp(comparator); //deep copy comparator
+    new_conditional -> rhs = rhs; //since rhs expression is already heap-allocated, can directly assign reference
     return new_conditional;
 }
 
-T_statement parse_statement() {
+T_statement parse_statement() { //statement = value or variable + equal sign + rhs (right hand side) operand
     // puts("86");
     T_token token = get_lookahead();
     // printf("token type: %d \n", token -> type);
@@ -179,17 +180,15 @@ T_statement parse_statement() {
 
 T_statement create_statement(T_val lhs, T_expression rhs) {
     T_statement new_statement = malloc(sizeof(*new_statement));
-    //deep copy lhs
-    new_statement -> var = deep_copy_val (lhs);
-    //since rhs expression is already heap-allocated, can directly assign reference
-    new_statement -> expression = rhs;
+    new_statement -> var = deep_copy_val (lhs); //deep copy lhs
+    new_statement -> expression = rhs; //since rhs expression is already heap-allocated, can directly assign reference
     return new_statement;
 }
 
 
 
-T_expression parse_expression() {
-    T_token lhs_token = get_lookahead();
+T_expression parse_expression() { //expression = lhs (left hand side) operand + operator + rhs (right hand side) operand
+    T_token lhs_token = get_lookahead(); 
     // puts("first");
     if (VAL != lhs_token -> type) parser_error();
     T_val lhs = lhs_token -> value.val;
@@ -214,7 +213,7 @@ T_expression parse_expression() {
     return create_expression(lhs, operator, rhs);
 }
 
-T_expression create_expression(T_val lhs, T_oper operator, T_expression rhs) {
+T_expression create_expression(T_val lhs, T_oper operator, T_expression rhs) { //create expression by replacing lhs, operator, and rhs with their respective deep copy
     // puts("inside create expression");
     T_expression new_expression = malloc(sizeof(*new_expression));
     // puts("141");
