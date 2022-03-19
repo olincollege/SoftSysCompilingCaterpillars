44911d3a31a517559b864ed41bf97ad52dd5d5f3
Rishita Sarin <rsarin@olin.edu>
2022-03-18 22:40:52 -0400

    commented parser.h

--- src/parser.h
+++ src/parser.h
@@ -13,7 +13,7 @@ typedef struct S_loop* T_loop;
 typedef struct S_conditional* T_conditional;
 typedef struct S_expression* T_expression;
 
-struct S_statement_list {
+struct S_statement_list { //contains a statement and next list
     enum {STATE, BRANCH, LOOP} type;
     union statements {
         T_statement statement;
@@ -23,34 +23,39 @@ struct S_statement_list {
     T_statement_list statement_list;
 };
 
-struct S_statement {
+struct S_statement { //contains an expression
     T_val var;
     T_expression expression;
 };
 
-struct S_branch {
+struct S_branch { //for conditionals
+//                branch 
+//           /      |       \
+//          /       |        \
+//condition     if statement     else statement
     T_conditional cond;
     T_statement_list if_exp;
     T_statement_list else_exp;
 };
 
-struct S_loop {
+struct S_loop { //for while loops
     T_conditional cond;
     T_statement_list exp;
 };
 
-struct S_conditional {
+struct S_conditional { //for conditions (lhs (left hand side) expression + comparator + rhs (right hand side) expression)
     T_expression lhs;
     T_comp comparator;
     T_expression rhs;
 };
 
-struct S_expression {
+struct S_expression { //for statements (expression = lhs operand + operator + rhs operand)
     T_val lhs;
     T_oper operator;
     T_expression rhs;
 };
 
+//parse through the lexed input
 T_statement_list parse_statement_list(int in_loop_while);
 T_statement parse_statement();
 T_expression parse_expression();
@@ -59,6 +64,8 @@ T_loop parse_while();
 T_branch parse_branch();
 T_token get_token();
 
+
+//create output based on parsed input
 T_statement_list create_statement_list(int type, T_statement statement, T_statement_list statement_list);
 T_statement create_statement(T_val lhs, T_expression rhs);
 T_expression create_expression(T_val lhs, T_oper operator, T_expression rhs);
@@ -73,4 +80,4 @@ void next_token();
 
 void parser_error();
 
-#endif
+#endif 
