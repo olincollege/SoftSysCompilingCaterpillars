499cb7e0e90e5238e292897972d5e83c12fb3a1a
Rishita Sarin <56645125+rishitasarin@users.noreply.github.com>
2022-03-22 03:15:27 -0400

    added comments

--- tutorials/Rishita/parser.c
+++ tutorials/Rishita/parser.c
@@ -2,7 +2,7 @@
 
 T_token lookahead_token;
 
-T_token get_lookahead() {
+T_token get_lookahead() { //look at next token
     return lookahead_token;
 }
 
@@ -11,8 +11,8 @@ void next_token() {
 }
 
 T_statement_list parse_statement_list() {
-    if (END != get_lookahead() ->kind) {
-        T_statement statement = parse_statement();
+    if (END != get_lookahead() ->kind) { //if the next token is not the end of the file, parse through the next statement else return null
+        T_statement statement = parse_statement(); 
         T_statement_list statement_list = parse_statement_list();
         return create_statement_list(statement, statement_list);
     }   else {
@@ -21,15 +21,15 @@ T_statement_list parse_statement_list() {
 }
 
 T_expression parse_expression() {
-    T_token operand1 = get_lookahead();
+    T_token operand1 = get_lookahead(); //if the first token is not a number, throw an error
     if (NUMBER != operand1->kind) parse_error();
     next_token();
 
-    T_token operator = get_lookahead();
+    T_token operator = get_lookahead(); //if the next token is not an operator, throw an error
     if (OPERATOR != operator->kind) parse_error();
     next_token();
 
-    T_token operand2 = get_lookahead();
+    T_token operand2 = get_lookahead(); //if the next token is not a number, throw an error
     if (NUMBER != operand2->kind) parse_error();
     next_token();
 
@@ -37,7 +37,7 @@ T_expression parse_expression() {
 }
 
 T_statement_list create_statement_list(T_statement statement, T_statement_list statement_list) {
-    T_statement_list new_list = malloc(sizeof(*new_list));
+    T_statement_list new_list = malloc(sizeof(*new_list)); //allocate memory as you add a new statement
     new_list->statement = statement;
     new_list->statement_list = statement_list;
     return new_list;
@@ -45,11 +45,11 @@ T_statement_list create_statement_list(T_statement statement, T_statement_list s
 
 T_statement create_statement(T_expression expression) {
     T_statement new_statement = malloc(sizeof(*new_statement));
-    new_statement->expression = expression;
+    new_statement->expression = expression; //a new statement is made up of an expression
     return new_statement;
 }
 
-T_expresion create_expression(T_token operand1, T_token operator, T_token operand2) {
+T_expresion create_expression(T_token operand1, T_token operator, T_token operand2) { //expression = operand1 + operator + operand2
     T_expression new_expresion = malloc(sizeof(*new_expresion));
     new_expresion->operand1 = operand1;
     new_expresion->operator = operator;
