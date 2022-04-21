266f32b81da8bc338611c1db03323316e1d046b4
Abitamim <abitamimbharmal@hotmail.com>
2022-04-20 20:36:07 -0400

    Add structs

--- includes/codegen.h
+++ includes/codegen.h
@@ -3,6 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct S_loop_wd* T_loop_wd;
+typedef struct S_branch_wd* T_branch_wd;
+
+struct S_loop_wd {
+    T_loop loop;
+    int head_dir;
+};
+
+struct S_branch_wd {
+    T_branch branch;
+    int else_dir;
+};
+
 void declare_vars(FILE* fptr, GHashTable* vars);
 void declare_var(gpointer key, gpointer value, gpointer user_data);
 void declare_constants(FILE* fptr, GArray* nums);
